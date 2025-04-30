1f485cdff256d55d97db76474b972a4dd1a120d2
efang5 <156245927+efang5@users.noreply.github.com>
2025-04-29 21:00:49 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,9 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if(typeof num1 != 'number' || typeof num3 != 'number'){
+        return false;
+    }
     if (add) {
         let result = 0;
 
@@ -25,8 +28,14 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if(!Array.isArray(prices)){
+        return false;
+    }
     let discounted = [];
     const length = prices.length;
+    if(length == 0){
+        return false;
+    }
     let discountedPrice = 0;
     for(let i = 0; i < length; i++) {
         discountedPrice = prices[i] * (1 - discount);
